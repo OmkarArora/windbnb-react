e4951e008e4f64d92f4815a5d27c2994f269e5f7
Omkar Arora <aroraomkar@gmail.com>
2021-01-10 19:35:32 +0530

    style: some padding updates

--- src/components/StaysGrid/StaysGrid.js
+++ src/components/StaysGrid/StaysGrid.js
@@ -3,7 +3,6 @@ import "./staysGrid.css";
 import StayData from "../data/stays.json";
 
 const StaysGrid = () => {
-  console.log(StayData);
   return (
     <div className="stays-grid">
       {StayData.map((item, index) => (
